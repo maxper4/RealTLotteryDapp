1d5d3a53c6c9a90efcc5baa2a0a29a01cf63534e
maxper4 <maxenceperion@gmail.com>
2023-09-18 11:42:17 +0200

    Updated meta informations

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "0x6E71cfbc04eF15db5CcB942f2e4C78efCDD02833";
-
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
